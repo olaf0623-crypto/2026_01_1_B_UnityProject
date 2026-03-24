9066aae9ce88d65b953411f0072a51b586ad7110
olaf0623-crypto <olaf0623@g.shingu.ac.kr>
2026-03-24 12:19:14 +0900

    [Feat] 프로젝트 정리

---
+++ Assets/Scenes/script/lucy.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class lucy : MonoBehaviour
+{
+    public int health = 100;
+    public float Timer = 1.0f;
+
+    public object Keycode { get; private set; }
+
+    void Start()
+    {
+        health = health + 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer = Timer - Timer.deltaTime;
+
+        if (Timer <= 0)
+        {
+            Timer = 1.0f;
+            health = health - 20;
+        }
+
+        if (Input.GetKeyDown(Keycode.Space))
+        {
+            health = health + 2;
+        }
+    }
+}
